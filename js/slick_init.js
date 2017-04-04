a43e084ba2242c1d2e04f7795d3361aa849ca4d0
Alex(is) <neverendingend@gmail.com>
2017-04-04 15:56:29 -0600

    slider hotfix

--- js/slick_init.js
+++ js/slick_init.js
@@ -5,26 +5,44 @@ $(document).ready(function(){
 	  slidesToShow: 5,
 	  slidesToScroll: 1,
 	  responsive: [
+	  	{
+	      breakpoint: 1234,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 1,
+	        infinite: false
+	      }
+	    },
 	    {
 	      breakpoint: 1024,
 	      settings: {
 	        slidesToShow: 3,
 	        slidesToScroll: 1,
-	        infinite: true,
+	        infinite: false
+	      }
+	    },
+	   	{
+	      breakpoint: 744,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1,
+	        infinite: false
 	      }
 	    },
 	    {
 	      breakpoint: 600,
 	      settings: {
 	        slidesToShow: 2,
-	        slidesToScroll: 1
+	        slidesToScroll: 1,
+	        infinite: false
 	      }
 	    },
 	    {
 	      breakpoint: 480,
 	      settings: {
 	        slidesToShow: 1,
-	        slidesToScroll: 1
+	        slidesToScroll: 1,
+	        infinite: false
 	      }
 	    }
 	  ]
